06de22d034f5deb06f390a6d17ef27c17713239c
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-09 12:04:28 -0700

    add function to render the todo items to the screen as list items

--- js/script.js
+++ js/script.js
@@ -32,3 +32,30 @@ function addTodo(item) {
         todoInput.value = ''; // clears the input value after
     }
 }
+
+// function to render given todos to screen
+function renderTodos(todos) {
+    // clear inside of ul with todo items class
+    todoItemsList.innerHTML = '';
+
+    todos.forEach(item => {
+        const checked = item.competed ? 'checked' : null;
+
+
+        const li = document.createElement('li');
+        li.setAttribute('class', 'item');
+        li.setAttribute('data-key', item.id);
+
+        if (item.completed === true) {
+            li.classList.add('checked');
+        }
+
+        li.innerHTML = `
+            <input type="checkbox" class="checkbox" ${checked}>
+            ${item.name}
+            <button class="delete-button">X</button>
+        `;
+
+        todoItemsList.append(li);
+    });
+}
