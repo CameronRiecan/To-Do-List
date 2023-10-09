91136bcdd7f1c5b5101a17f9f2d2622fe3a1a87b
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-10-09 11:50:54 -0700

    add function for adding todo items to the array, rendering it, and then clearing the input

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
 // Main JavaScript file for To Do List project
 
+// select everything
+// select the todo-form
+const todoForm = document.querySelector('.todo-form');
+// select the input box
+const todoInput = document.querySelector('.todo-input');
+// select the <ul> with class="todo-items"
+const todoItemsList = document.querySelector('.todo-items');
+
+// array that stores every todo
+let todos  = [];
+
+// event listener on form for submit event
+todoForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    addTodo(todoInput.value);
+});
+
+//function to add todo
+function addTodo(item) {
+    if (item !== '') {
+        const todo = {
+            id: Date.now(),
+            name: item,
+            completed: false
+        };
+
+        todos.push(todo);
+        renderTodos(todos);
+
+        todoInput.value = ''; // clears the input value after
+    }
+}
